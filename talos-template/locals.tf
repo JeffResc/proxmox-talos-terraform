ae4600f25d09279cb641bf058ef702ab48006797
Jeff Rescignano <jeff@jeffresc.dev>
2025-07-08 00:17:46 -0400

    feat: configurable vm resources

--- talos-template/locals.tf
+++ talos-template/locals.tf
@@ -13,13 +13,13 @@ locals {
   worker_disk_size       = 50
   
   # Memory configurations (in MB)
-  controlplane_memory = 4096
-  worker_memory       = 8192
+  controlplane_memory = var.controlplane_memory
+  worker_memory       = var.worker_memory
   
   # CPU configurations
-  controlplane_cpu_cores = 4
-  worker_cpu_cores       = 8
-  cpu_type              = "x86-64-v2-AES"
+  controlplane_cpu_cores = var.controlplane_cpu_cores
+  worker_cpu_cores       = var.worker_cpu_cores
+  cpu_type              = var.cpu_type
   
   # Network configuration
   network_bridge = "vmbr0"
